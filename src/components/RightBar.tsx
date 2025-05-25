5d2bb8fb284926b25401ac2373e4b3521e0d7aa5
canemrecn <emrecancnzytnl@gmail.com>
2025-05-25 16:44:49 +0300

    asdfasdfasdfasd

--- src/components/RightBar.tsx
+++ src/components/RightBar.tsx
@@ -12,17 +12,26 @@ import Search from "./Search";
 
 export default function RightBar() {
   return (
-    <div className="
-    hidden 
-    lg:flex 
-    flex-col 
-    fixed 
-    top-0 
-    right-0 
-    w-94 
-    justify-between 
-    z-50">
-      <Search/>
+    <div
+      className="
+        hidden 
+        lg:flex 
+        flex-col 
+        fixed 
+        top-0 
+        right-0 
+        h-screen
+        w-80 
+        bg-gradient-to-b from-gray-900 to-gray-800 
+        rounded-l-xl
+        shadow-xl 
+        z-50 
+        py-6 
+        px-4 
+        gap-6
+      "
+    >
+      <Search />
       <Arrangement />
       <Notes />
     </div>
