be514821f11602d03e5e36905fd2d820e8cbe8ab
canemrecn <emrecancnzytnl@gmail.com>
2025-05-29 00:08:57 +0300

    asdfasdfsdafasd

--- src/app/hashtag/[tag]/page.tsx
+++ src/app/hashtag/[tag]/page.tsx
@@ -1,11 +1,6 @@
 import React from "react";
-import { notFound } from "next/navigation";
 import Post from "@/components/Post";
 import { PostData } from "@/components/Post";
-
-type Props = {
-  params: { tag: string };
-};
 
 async function fetchPostsByHashtag(tag: string): Promise<PostData[]> {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/hashtag/${tag}`, {
@@ -19,7 +14,7 @@ async function fetchPostsByHashtag(tag: string): Promise<PostData[]> {
   return data.posts;
 }
 
-export default async function HashtagPage(props: Props) {
+export default async function HashtagPage(props: any) {
   const tag = decodeURIComponent(props.params.tag);
   const posts = await fetchPostsByHashtag(tag);
 
